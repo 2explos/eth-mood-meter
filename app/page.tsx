1c329b8cd711e771fda68597f58bd246807665a1
Ara <deuxexplorateurs@icloud.com>
2025-10-22 19:16:37 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import { MoodWidget } from '@/components/MoodWidget';
 export default function Home() {
   return (
     <main>
-      {/* Monte le hook qui force sdk.actions.ready() */}
       <WarpcastReady />
       <MoodWidget />
     </main>
